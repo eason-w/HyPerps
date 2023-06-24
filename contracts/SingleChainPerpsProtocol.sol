dac63b052677f2a4ea47c03b7decb9d097df0bf2
Eason Wu <real.ewu4@gmail.com>
2023-06-24 11:57:47 -0400

    Update SingleChainPerpsProtocol.sol

--- contracts/SingleChainPerpsProtocol.sol
+++ contracts/SingleChainPerpsProtocol.sol
@@ -1,7 +1,6 @@
 pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
-import "./pythTestnetPriceFetcher.sol";
 import "scaffold-eth/node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "scaffold-eth/node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
