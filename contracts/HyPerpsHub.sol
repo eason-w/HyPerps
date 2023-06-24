47615ed7cdac24c31f2962ab26812ed4231545db
Eason Wu <real.ewu4@gmail.com>
2023-06-24 16:00:28 -0400

    HyPerps contracts creation
    Using hub and spoke model implementation

--- contracts/HyPerpsHub.sol
+++ contracts/HyPerpsHub.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "scaffold-eth/node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "scaffold-eth/node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "scaffold-eth/node_modules/@hyperlane-xyz/core/contracts/interfaces/IMessageRecipient.sol";
+import "scaffold-eth/node_modules/@hyperlane-xyz/core/contracts/interfaces/IMailbox.sol";
 
 contract HyPerpsHub is Ownable{
 }
---
+++ contracts/HyPerpsSpoke.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "scaffold-eth/node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "scaffold-eth/node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "scaffold-eth/node_modules/@hyperlane-xyz/core/contracts/interfaces/IMessageRecipient.sol";
+import "scaffold-eth/node_modules/@hyperlane-xyz/core/contracts/interfaces/IMailbox.sol";
+
+contract HyPerpsHub is Ownable{
+}
