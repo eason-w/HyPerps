caf973854cda9a4e95cb1abc36805f06d5857885
Eason Wu <real.ewu4@gmail.com>
2023-06-24 16:57:57 -0400

    add interchain gas paymaster

--- contracts/HyPerpsHub.sol
+++ contracts/HyPerpsHub.sol
@@ -5,6 +5,7 @@ import "scaffold-eth/node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "scaffold-eth/node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "scaffold-eth/node_modules/@hyperlane-xyz/core/contracts/interfaces/IMessageRecipient.sol";
 import "scaffold-eth/node_modules/@hyperlane-xyz/core/contracts/interfaces/IMailbox.sol";
+import "scaffold-eth/node_modules/@hyperlane-xyz/core/contracts/interfaces/IInterchainGasPaymaster.sol";
 
 contract HyPerpsHub is Ownable{
 }
